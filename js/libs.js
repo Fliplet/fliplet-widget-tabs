5cb0b5f829b564380b7442632b465249b66b3f04
Hugo Carneiro <hello@thegeekdeveloper.com>
2020-06-02 19:02:18 +0100

    Save and render dropped components

--- js/libs.js
+++ js/libs.js
@@ -6,6 +6,9 @@
 Fliplet.Registry.set('tabs:1.0:core', function (element, data) {
   var mode = Fliplet.Env.get('mode');
   var isDev = Fliplet.Env.get('development');
+  var WIDGET_INSTANCE_DATA = 'data-fl-widget-instance';
+  var WIDGET_INSTANCE_SELECTOR = '[' + WIDGET_INSTANCE_DATA + ']';
+  var WIDGET_INSTANCE_ID_DATA = 'id';
 
   data.tabs = data.tabs || []
 
@@ -26,6 +29,78 @@ Fliplet.Registry.set('tabs:1.0:core', function (element, data) {
     var addNewTabHTML = addNewTabTemplate();
 
     $('.fl-tabs-container .nav-tabs').append(addNewTabHTML);
+  }
+
+  function replaceWidgetInstances($html) {
+    $html.find(WIDGET_INSTANCE_SELECTOR).replaceWith(function () {
+      var widgetInstanceId = $(this).data(WIDGET_INSTANCE_ID_DATA);
+
+      return '{{{widget ' + widgetInstanceId + '}}}';
+    });
+
+    return $html;
+  }
+
+  function studioEventHandler() {
+    Fliplet.Studio.onEvent(function (event) {
+      var eventDetail = event.detail;
+
+      if (eventDetail.id !== data.id) {
+        return;
+      }
+
+      switch (eventDetail.type) {
+        case 'saveWidgetRichContent':
+          var activeTabIndex = $(element).find('.tab-content .tab-pane.active').index();
+          var $html =
+            $('<div>' + $(element).find('.tab-content .tab-pane.active').html() + '</div>');
+
+          Fliplet.Hooks.run('beforeSavePageContent', $html.html())
+            .then(function (html) {
+              if (!Array.isArray(html) || !html.length) {
+                html = [$html.html()];
+              }
+
+              return html[html.length - 1];
+            })
+            .then(function (html) {
+              $html = $('<div>' + html + '</div>');
+
+              if (Fliplet.Helper) {
+                Fliplet.Helper.restoreStateForHtml($html);
+              }
+
+              $html = replaceWidgetInstances($html);
+
+              var htmlString = $html.html();
+
+              data.tabs[activeTabIndex].html = htmlString;
+
+              Fliplet.Studio.emit('page-saving');
+
+              return Fliplet.API.request({
+                url: 'v1/widget-instances/' + eventDetail.id,
+                method: 'PUT',
+                data: data
+              })
+            })
+            .then(function () {
+              // Emit event to Studio to get the updated DOM
+              Fliplet.Studio.emit('update-dom');
+
+              Fliplet.Studio.emit('page-saved');
+            })
+            .catch(function (error) {
+              Fliplet.Studio.emit('page-saved', {
+                isError: true,
+                error: error
+              });
+            });
+          break;
+        default:
+          break;
+      }
+    })
   }
 
   function attachObeservers() {
@@ -56,6 +131,7 @@ Fliplet.Registry.set('tabs:1.0:core', function (element, data) {
   function initialize() {
     registerHelpers();
     insertAddNewTab();
+    studioEventHandler();
     attachObeservers();
   }
 
